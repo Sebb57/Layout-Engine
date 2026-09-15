c2a6e262f07d748c5fe6024e307a86f0a1ca617c
SkyReox <alexis.roufast-chalada@epitech.eu>
2026-09-15 14:15:42 +0200

    feat: Event Structure
    may be changed later

--- include/Component.hpp
+++ include/Component.hpp
@@ -37,7 +37,74 @@ struct Transform {
     Rect Size;
 };
 
+struct Event {
+    enum class Type {
+        None,
+        Closed,
+        KeyPressed,
+        KeyReleased,
+        TextEntered,
+        MouseWheelScrolled,
+        MouseMoved,
+        MouseButtonPressed
+    };
+
+    enum class Key {
+        Unknown,
+        Escape,
+        Space,
+        Shift,
+        Control,
+        Alt,
+        Left,
+        Right,
+        Up,
+        Down,
+        Delete,
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H,
+        I,
+        J,
+        K,
+        L,
+        M,
+        N,
+        O,
+        P,
+        Q,
+        R,
+        S,
+        T,
+        U,
+        V,
+        W,
+        X,
+        Y,
+        Z,
+    };
+
+    enum class MouseButton {
+        None,
+        Left,
+        Right,
+        Middle
+    };
+
+    Type type = Type::None;
+    Key key = Key::Unknown;
+    char keyChar = '\0';
+    MouseButton mouseButton = MouseButton::None;
+    double wheelDelta = 0.0;
+    int mouseX = 0;
+    int mouseY = 0;
+};
+
 }
 
 #endif /* COMPONENT_HPP_ */
-
