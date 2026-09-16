90731d9841e681ea421be1303d2817c2fb37d401
Sebb57 <sebdoussang@gmail.com>
2026-09-16 13:09:04 +0200

    chore: removed test binary from repo and added it to gitignore + better test file

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,13 @@
 int main()
 {
     Layout::Layout layout;
-    layout.load("example.layout");
-    std::cout << "Layout Engine" << std::endl;
+
+    try {
+        layout.load("example.layout");
+        std::cout << "Layout Engine" << std::endl;
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
     return 0;
 }
